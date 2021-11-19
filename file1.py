2b026beef4366b2a465c7a1582ad193692eb3483
Chetan Kushwaha <ckushwaha536@gmail.com>
2021-11-19 11:14:39 +0530

    generate_data_feature_added

--- file1.py
+++ file1.py
@@ -7,4 +7,13 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
-    
+
+# generate random data for training
+def generate_data(n):
+    data = []
+    for i in range(n):
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+        label = 1 if x**2 + y**2 < 1 else 0
+        data.append((x, y, label))
+    return data
